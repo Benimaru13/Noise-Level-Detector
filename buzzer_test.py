71d5aae42694d97586c056b419a79dfbdb73e6c5
Sarah Ye <sarahye616@gmail.com>
2026-06-12 14:35:59 -0700

    Add print statement to buzzer_test.py

--- buzzer_test.py
+++ buzzer_test.py
@@ -3,8 +3,12 @@ from time import sleep
 
 buzzer = Buzzer(10)
 
+print("Buzzer Test Started!")
 while True:
     buzzer.on()
+    print("Buzzer on!")
     sleep(1)
     buzzer.off()
+    print("Buzzer off!")
     sleep(1)
+    
